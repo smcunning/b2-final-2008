750c447ddfcb564d0f7c7ef0ea0f9cc8ae6e8fe4
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-11-09 09:59:07 -0700

    Update spec to test for find unique universities

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -15,5 +15,17 @@ describe Hospital, type: :model do
 
       expect(@hospital.total_doctors).to eq(4)
     end
+
+    it "find_unique_universities" do
+      @hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+      @doctor = Doctor.create!(name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", hospital_id: @hospital.id)
+      @doctor_2 = Doctor.create!(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", hospital_id: @hospital.id)
+      @doctor_3 = Doctor.create!(name: "Miranda Bailey", specialty: "General Surgery", education: "Johns Hopkins University", hospital_id: @hospital.id)
+      @doctor_4 = Doctor.create!(name: "Derek McDreamy Shepherd", specialty: "Attending Surgeon", education: "University of Pennsylvania", hospital_id: @hospital.id)
+
+      expected = ["Harvard University", "Johns Hopkins University", "University of Pennsylvania"]
+
+      expect(@hospital.find_unique_universities).to eq(expected)
+    end
   end
 end
