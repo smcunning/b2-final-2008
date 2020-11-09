1d0083561d676b5cb612157967f8c563b9a14767
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-11-09 10:48:35 -0700

    Refactor to include strong params

--- app/controllers/patient_doctors_controller.rb
+++ app/controllers/patient_doctors_controller.rb
@@ -1,8 +1,13 @@
 class PatientDoctorsController < ApplicationController
 
   def destroy
-    @pd = PatientDoctor.where(patient_id: params[:id])
+    @pd = PatientDoctor.where(patient_id: patient_params[:id])
     @pd.first.delete
     redirect_to request.referrer
+  end
+
+  private
+  def patient_params
+    params.permit(:id)
   end
 end
