6b78fc055ca2639f1288fdf4393522a73bcf1d08
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-11-09 10:42:00 -0700

    Create names by age class method

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,8 @@
 class Patient < ApplicationRecord
   has_many :patient_doctors
   has_many :doctors, through: :patient_doctors
+
+  def self.names_by_age
+    order("age desc").pluck(:name)
+  end
 end
