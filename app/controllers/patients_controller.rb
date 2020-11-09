504ef47345d45b4b9cc7a5237caa4d687b3407a4
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-11-09 10:23:54 -0700

    Update controller with patient names by age

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,7 @@
 class PatientsController < ApplicationController
 
   def index
-    @patients = Patient.all
+    patients = Patient.all
+    @patient_names_by_age = patients.patient_names_by_age
   end
 end
