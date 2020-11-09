7ec007000821427f5732fe6d3fef776c5c9f47a0
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-11-09 10:06:29 -0700

    Create route to patient_doctors destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :doctors, only: [:show]
+
   resources :hospitals, only: [:show]
-  
+
+  delete "/doctor/patients/:id", to: "patient_doctors#destroy"
 end
