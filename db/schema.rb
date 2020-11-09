70dec07122b7933d37026591e8d1a6e86c9346bc
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-11-09 09:25:58 -0700

    Migrate PatientDoctors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_162239) do
+ActiveRecord::Schema.define(version: 2020_11_09_162511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,10 +27,19 @@ ActiveRecord::Schema.define(version: 2020_11_09_162239) do
     t.string "name"
   end
 
+  create_table "patient_doctors", force: :cascade do |t|
+    t.bigint "doctor_id"
+    t.bigint "patient_id"
+    t.index ["doctor_id"], name: "index_patient_doctors_on_doctor_id"
+    t.index ["patient_id"], name: "index_patient_doctors_on_patient_id"
+  end
+
   create_table "patients", force: :cascade do |t|
     t.string "name"
     t.integer "age"
   end
 
   add_foreign_key "doctors", "hospitals"
+  add_foreign_key "patient_doctors", "doctors"
+  add_foreign_key "patient_doctors", "patients"
 end
